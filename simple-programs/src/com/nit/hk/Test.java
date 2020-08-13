692b2b91f0840d78a3f8742598dc527d3ea1fe67
ashru344 <ashru.sahoo06@gmail.com>
2020-08-13 13:14:54 +0530

    1st update
    sopln changes

--- simple-programs/src/com/nit/hk/Test.java
+++ simple-programs/src/com/nit/hk/Test.java
@@ -3,6 +3,6 @@ package com.nit.hk;
 public class Test {
 
 	public static void main(String[] args) {
-		System.out.println("Hello world program");
+		System.out.println("Hello world program ! It's my First commit to the Git");
 	}
 }
