e46b9bdeccf956cde0f1e3bfd0ba37b9bbf57f6c
Biswa Bhusan Sahoo <Biswa.Sahoo@tangoe.com>
2020-08-13 13:45:45 +0530

    Added Calculation Service

---
+++ simple-programs/src/com/nit/hk/CalculationTest.java
@@ -0,0 +1,19 @@
+package com.nit.hk;
+
+public class CalculationTest
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello world program ! It's my First commit to the Git" );
+        int sum = add( 5, 6 );
+        System.out.println( "Sum :: " + sum );
+
+    }
+
+    public static int add( int a,
+                           int b )
+    {
+        return a + b;
+    }
+}
--- simple-programs/src/com/nit/hk/Test.java
+++
@@ -1,8 +0,0 @@
-package com.nit.hk;
-
-public class Test {
-
-	public static void main(String[] args) {
-		System.out.println("Hello world program ! It's my First commit to the Git");
-	}
-}
